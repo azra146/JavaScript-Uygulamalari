ed4406d6ad7767868bfff7dadfd3725ac6118d19
azra uçar <azra.ucar@ogr.gelisim.edu.tr>
2025-10-10 16:27:25 +0300

    Add files via upload

---
+++ diyalog_kutulari.js
@@ -0,0 +1,9 @@
+//alert("MERHABA AZRA");
+
+/*let isim=prompt("İSMİNİZİ GİRİN:");
+let soyad=prompt("SOYADINIZI GİRİN:");
+console.log("İSMİNİZ:"+ isim );
+console.log("SOYADINIZ:"+ soyad);*/
+
+//let sonuc=confirm("SİLMEK İSTEDİĞİNİZE EMİN MİSİNİZ?");//
+Swal.fire(" MERHABA AZRA");
