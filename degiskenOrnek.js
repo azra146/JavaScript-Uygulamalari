d07ab062eaa5b552e1b41af894e000d27e867b63
azra uçar <azra.ucar@ogr.gelisim.edu.tr>
2025-10-10 16:39:58 +0300

    Add files via upload

---
+++ degiskenOrnek.js
@@ -0,0 +1,15 @@
+console.log("EKRAN ÖZETİ");
+var ad="Efe";
+console.log("ÖĞRENCİ: "+ ad);
+
+let okul;
+console.log(okul);
+okul="Gökkuşağı Koleji";
+console.log("OKUL: "+ okul);
+
+let program="Sayısal";
+program="Sözel";
+console.log("PROGRAM: "+ program);
+
+let sifre="deneme";
+console.log("ŞİFRE: "+ sifre);
