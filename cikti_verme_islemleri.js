829ad62dd5878802ffd332b9f313c7073b343c73
azra uçar <azra.ucar@ogr.gelisim.edu.tr>
2025-10-10 16:27:06 +0300

    Add files via upload

---
+++ cikti_verme_islemleri.js
@@ -0,0 +1,16 @@
+console.log("AZRA UÇAR");
+console.log(15);
+console.log(3.14); 
+console.log(true);
+
+console.dir(window);  
+console.dir(window.document.location.host);
+console.dir(window.document.documentURI);
+console.dir(window.location.port);
+
+
+document.writeln("AZRA UÇAR" +"<br>");
+document.writeln(18 +"<br>");
+document.writeln(3.14 +"<br>");
+document.writeln(true +"<br>");
+document.writeln(null +"<br>");
