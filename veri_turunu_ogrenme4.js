c9871c1aa45120b178be9b6a01351fe61f30b198
azra uçar <azra.ucar@ogr.gelisim.edu.tr>
2025-10-24 16:20:02 +0300

    Add files via upload

---
+++ veri_turunu_ogrenme4.js
@@ -0,0 +1,9 @@
+let sayi="15";
+console.log(sayi , " DEĞERİ: ", typeof(sayi) );
+let sayi1=parseInt(sayi);
+console.log(sayi1 , " DEĞERİ: ", typeof(sayi1) );
+let sayi2=11.5;
+console.log(sayi2 , " DEĞERİ: ", typeof(sayi2) );
+let sayi3=55;
+let sayi4=sayi3.toString();
+console.log(sayi4 , " DEĞERİ: ", typeof(sayi4) );
